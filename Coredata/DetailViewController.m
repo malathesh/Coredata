237edf3129b1cde09d0f8b2217be367762784309
malathesh.helvar <malathesh.helvar@gmail.com>
2016-01-11 15:32:30 +0530

    Changes in comment section

---
+++ Coredata/DetailViewController.m
@@ -0,0 +1,67 @@
+
+//
+//  DetailViewController.m
+//  Coredata
+//
+//  Created by Malathesh Helvar on 12/29/15.
+//  Copyright © 2015 Ganesh Hebbal Subash. All rights reserved.
+//
+
+#import "DetailViewController.h"
+#import "AppDelegate.h"
+@interface DetailViewController ()
+@property(nonatomic,strong)NSManagedObjectContext *managedObjectContext;
+@end
+
+@implementation DetailViewController
+
+//I have added the hole project in git
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+- (NSManagedObjectContext *)managedObjectContext {
+    NSManagedObjectContext *context = nil;
+    id delegate = [[UIApplication sharedApplication] delegate];
+    if ([delegate performSelector:@selector(managedObjectContext)]) {
+        context = [delegate managedObjectContext];
+    }
+    return context;
+}
+- (IBAction)cancel:(UIBarButtonItem *)sender {
+      [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (IBAction)Save:(UIBarButtonItem *)sender {
+    NSManagedObjectContext *context = [self managedObjectContext];
+    
+    // Create a new managed object
+    NSManagedObject *newDevice = [NSEntityDescription insertNewObjectForEntityForName:@"Device" inManagedObjectContext:context];
+    [newDevice setValue:self.name.text forKey:@"name"];
+    [newDevice setValue:self.version.text forKey:@"version"];
+    [newDevice setValue:self.company.text forKey:@"company"];
+    
+    NSError *error = nil;
+    // Save the object to persistent store
+    if (![context save:&error]) {
+        NSLog(@"Can't Save! %@ %@", error, [error localizedDescription]);
+    }
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+@end
